780796a4bd9ad45e30a05e4116157ab597755232
ripe0x <unripeengine@gmail.com>
2022-07-25 17:08:16 -0700

    functional beta version

---
+++ README.md
@@ -0,0 +1,5 @@
+# ⌐◨-◨.app
+![Image](assets/copy-paste-noggles.gif)
+
+### Quickly copy unicode noggles to your clipboard
+Supported by Nouns DAO through [Prop House #679](https://prop.house/proposal/679).
---
+++ noggles.js
@@ -0,0 +1,67 @@
+const { app, Menu, Tray } = require('electron');
+const path = require('path');
+const { BrowserWindow } = require('electron');
+
+const { menubar } = require('menubar');
+const { clipboard, nativeTheme } = require('electron');
+
+const nogglesList = [
+	'⌐◧-◧',
+	'⌐◨-◨',
+	'⌐⁞⁚⁞-⁞⁚⁞',
+	'⌐◫-◫',
+	'⌐◪-◪',
+	'⌐◮-◮',
+	'⌐♥-♥',
+	'⌐🄶-🄼',
+	'⌐ⓝ°ⓒ',
+];
+
+app.on('ready', () => {
+	const getTrayIcon = (isDark = nativeTheme.shouldUseDarkColors) => {
+		return path.join(EXTRA_RESOURCES_PATH, `icon${isDark ? '-dark' : ''}.png`);
+	};
+
+	const EXTRA_RESOURCES_PATH = app.isPackaged
+		? path.join(process.resourcesPath, 'assets')
+		: path.join(__dirname, 'assets');
+
+	const quitApp = () => {
+		app.quit();
+	};
+
+	const copyNoggles = (text) => {
+		clipboard.writeText(text);
+	};
+	const menuItems = nogglesList.map((nog, i, { length }) =>
+		length - 2 === i
+			? {
+					type: 'separator',
+			  }
+			: length - 1 === i
+			? {
+					label: '⌐×-×',
+					click: function () {
+						quitApp();
+					},
+			  }
+			: {
+					label: nog,
+					click: function () {
+						copyNoggles(nog);
+					},
+			  }
+	);
+	const tray = new Tray(getTrayIcon());
+	const contextMenu = Menu.buildFromTemplate(menuItems);
+	tray.setContextMenu(contextMenu);
+	const mb = menubar({
+		tray,
+	});
+
+	mb.on('ready', () => {
+		// Don't open browser window
+		tray.removeAllListeners();
+		nativeTheme.on('updated', () => mb.tray.setImage(getTrayIcon()));
+	});
+});
